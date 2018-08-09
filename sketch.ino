b4301bc68c24234c125f212c2577274c7e1c0fa9
Thomas Cooper <tc@coope.biz>
2018-08-09 21:31:20 +0100

    Create sketch.ino

---
+++ sketch.ino
@@ -0,0 +1,51 @@
+
+void setup() {
+  Serial.begin(115200);
+  ledcSetup(0, 1E5, 12);
+  ledcAttachPin(25, 0); // Change 25 to 26 as required
+
+}
+
+void loop() {
+
+  char Number = 0;
+  uint8_t octave = 4; // Change this for higher or lower pitch
+  if (Serial.available())
+  {
+    Number = Serial.read();
+  }
+  switch (Number)
+  {
+    case '1':
+      Serial.println("OPTION 1: C");
+
+
+      ledcWriteNote(0, NOTE_C, octave);  // We've used the musical scale.  If you look at the link in the readme, you can also specify the frequency if you wish.
+      delay(500);
+      ledcWrite(0, 0);
+
+      break;
+
+    case '2':
+      Serial.println("OPTION 2: D");
+
+      ledcWriteNote(0, NOTE_D, octave);
+      delay(500);
+      ledcWrite(0, 0);
+
+      break;
+
+    case '3':
+      Serial.println("OPTION 3: E");
+
+      ledcWriteNote(0, NOTE_E, octave);
+      delay(500);
+      ledcWrite(0, 0);
+
+      break;
+
+  }
+
+  Number = 0;
+
+}
